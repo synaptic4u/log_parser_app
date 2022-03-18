4ebf1bd3241031da04a89c0aa207c491b80a54c3
Emile <55599624+synaptic4u@users.noreply.github.com>
2022-03-18 16:19:36 +0200

    Update Tables.php
    Reverted back to insert in sql. replace was taking too long and would create errors.

--- LogParserApp/app/src/Tables/Tables.php
+++ LogParserApp/app/src/Tables/Tables.php
@@ -29,6 +29,7 @@ class Tables
         // print_r(json_encode($table_list, JSON_PRETTY_PRINT).PHP_EOL);
     }
 
+    
     public function readTableColumns($table): mixed
     {
         $columns = [];
